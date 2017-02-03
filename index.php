68471e8d1fb266928af5d050ebfe3cf794f0ca2f
Clément <illuclem@gmail.com>
2017-02-03 16:53:23 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php ?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Les musées de nos Régions</title>
+        <link rel="stylesheet" href="css/materialize.min.css">
+        <script src="js/materialize.min.js"></script>
+        <style>
+            #image {
+                width: 100%;
+                height: 450px;
+                background-image: url("img/header.jpg");
+                background-repeat: no-repeat;
+                background-size: 100% 450px;
+            }
+
+            .nav-wrapper {
+                background: #FEB980 !important;
+            }
+            nav li a {
+                color: #373737;
+            }
+            nav li a:hover {
+                background: #FD7A71;
+            }
+        </style>
+    </head>
+    <body>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo center">Logo</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="sass.html">Accueil</a></li>
+                    <li><a href="badges.html">Liste des musées</a></li>
+                    <li><a href="collapsible.html">Ajouter un musée</a></li>
+                    <li></li>
+                </ul>
+            </div>
+        </nav>
+        <div id="image"></div>
+        <div class="container">
+            <div class="row">
+                lol
+            </div>
+        </div>
+    </body>
+</html>
