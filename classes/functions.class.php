a8c95767f8eda2b6354ccccc28315a56713ce472
Clément <illuclem@gmail.com>
2017-02-07 13:13:17 +0100

    Update functions.class.php

--- classes/functions.class.php
+++ classes/functions.class.php
@@ -9,18 +9,52 @@ class Functions {
         return $fetch;
     }
 
-    public function getImage($musee) {
-        include_once("html/simple_html_dom.php");
-        $html = file_get_html('https://www.google.fr/search?q=' . urlencode($musee) . '&biw=1366&bih=659&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjg5829t_vRAhWLwBQKHWKMAfIQ_AUIBygC');
-        $image = $html->find('img', 0)->src;
-        echo '<img src="' . $image . '" style="float: right;" />';
-    }
-
     public function text2Link($string) {
-        if (substr($string, 0, 4) == "www") {
-            $string = "http://{$reg}";
+        if (substr($string, 0, 3) == "www") {
+            $string = "http://{$string}";
         }
         $reg  = preg_replace('@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@', '<a href="$1" target="_blank">$1</a>', $string);
         return $reg;
+    }
+
+    public function getGoogle($localisation) {
+        $apiKey = "AIzaSyBSPF5q5m2uk0mcsHl48SFcCukZ7ksQY_E";
+        $url    = "https://maps.googleapis.com/maps/api/geocode/json?address=" . str_replace(" ", "+", $localisation)  . "&key=" . $apiKey;
+        $json   = json_decode(file_get_contents($url), true);
+        return [$json[0]["geometry"]["localisation"]["lat"], $json[0]["geometry"]["localisation"]["lng"]];
+    }
+
+    public function loadMap($ville) {
+        $frame  = "<iframe ";
+        $frame  .= 'src="//www.google.com/maps/embed/v1/place?q=' . str_replace(" ", "+", $ville) . '&zoom=17&key=AIzaSyBSPF5q5m2uk0mcsHl48SFcCukZ7ksQY_E"';
+        $frame  .= '></iframe>';
+        return $frame;
+    }
+
+    public function searchByDep($string) {
+        global $db;
+        $query  = $db->pdo->query("SELECT * FROM musee WHERE nom_dep LIKE '%{$string}%'");
+        $fetch  = $query->fetchAll();
+        return $fetch;
+    }
+
+    public function searchByCdp($string) {
+        global $db;
+        $array  = [];
+        $query  = $db->pdo->query("SELECT * FROM musee;");
+        $fetch  = $query->fetchAll();
+        for ($i = 0; $i < sizeof($fetch); $i++) {
+            if (substr($fetch[$i]["cp"], 0, 2) == substr($string, 0, 2)) {
+                array_push($array, $fetch[$i]);
+            }
+        }
+        return $array;
+    }
+
+    public function searchAll($string) {
+        global $db;
+        $query  = $db->pdo->query("SELECT * FROM musee WHERE nom_du_musee LIKE '%{$string}%'");
+        $fetch  = $query->fetchAll();
+        return $fetch;
     }
 }
