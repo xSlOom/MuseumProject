6db835a9f9fb63783ea1138d1a41f957b351cc04
Clément <illuclem@gmail.com>
2017-02-07 21:05:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -82,7 +82,6 @@ $fl = new Functions();
         $(function() {
             var adress = $("#adress" + nid + "").html().split('</strong>')[1].replace(/ /g, '+');
             var musee = $("#musee" + nid + "").html();
-            console.log(musee);
             $.ajax({
                 url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + adress + "&key=AIzaSyBSPF5q5m2uk0mcsHl48SFcCukZ7ksQY_E",
                 success: function(result){
